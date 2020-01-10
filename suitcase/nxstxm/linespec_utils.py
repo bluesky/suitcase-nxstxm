0716b5e3d9d1f2e0f74d5097ddc8b0db2c352759
Russ <russ.berg@lightsource.ca>
2020-01-10 10:57:04 -0600

    fixed so that data has proper dimensions

--- suitcase/nxstxm/linespec_utils.py
+++ suitcase/nxstxm/linespec_utils.py
@@ -20,8 +20,6 @@ import suitcase.nxstxm.nx_key_defs as nxkd
 MARK_DATA = False
 
 
-#     parent.modify_line_spectra_ctrl_str_attrs(cntrl_nxgrp, doc)
-#     parent.modify_line_spectra_ctrl_data_grps(cntrl_nxgrp, doc)
 
 def modify_line_spectra_ctrl_data_grps(parent, nxgrp, doc, scan_type):
     '''
@@ -61,7 +59,7 @@ def modify_line_spectra_ctrl_data_grps(parent, nxgrp, doc, scan_type):
             ydata = np.array(dct_get(rois, SPDB_YSETPOINTS), dtype=np.float32)
         else:
             # use actual data
-            # xdata is teh first xnpoints
+            # xdata is the first xnpoints
             xdata = np.array(parent._data['primary'][x_src][uid]['data'][0:xnpoints], dtype=np.float32)
             # ydata is every ynpoint
             ydata = np.array(parent._data['primary'][y_src][uid]['data'][0::ynpoints], dtype=np.float32)
@@ -94,24 +92,9 @@ def modify_line_spectra_ctrl_data_grps(parent, nxgrp, doc, scan_type):
         if (resize_data):
             sr_data = np.resize(sr_data, (ttlpnts,))
 
-    _dataset(nxgrp, 'data', np.reshape(sr_data, (1, xnpoints, num_ev_points)), 'NX_NUMBER')
+    _dataset(nxgrp, 'data', np.reshape(sr_data, (xnpoints, num_ev_points)), 'NX_NUMBER')
+    _string_attr(nxgrp, 'axes', ['line_position', 'energy'])
 
-    modify_line_spectra_ctrl_str_attrs(parent, nxgrp, doc)
-
-
-def modify_line_spectra_ctrl_str_attrs(parent, nxgrp, doc):
-    '''
-
-    :param nxgrp:
-    :param doc:
-    :return:
-    '''
-    rois = parent.get_rois_from_current_md(doc['run_start'])
-    x_posnr_nm = parent.fix_posner_nm(dct_get(rois, SPDB_XPOSITIONER))
-    y_posnr_nm = parent.fix_posner_nm(dct_get(rois, SPDB_YPOSITIONER))
-    # z_posnr_nm = parent.fix_posner_nm(dct_get(rois, SPDB_ZPOSITIONER))
-
-    _string_attr(nxgrp, 'axes', ['energy', y_posnr_nm, x_posnr_nm])
 
 def modify_line_spectra_nxdata_group(parent, data_nxgrp, doc, scan_type):
     '''
@@ -146,14 +129,14 @@ def modify_line_spectra_nxdata_group(parent, data_nxgrp, doc, scan_type):
         ydata = np.array(dct_get(rois, SPDB_YSETPOINTS), dtype=np.float32)
     else:
         prim_data_lst = parent._data['primary'][x_src][uid]['data']
-        if (len(prim_data_lst) < ttl_pnts):
+        if (len(prim_data_lst) < ttlpnts):
             resize_data = True
             # scan was aborted so use setpoint data here
             xdata = np.array(dct_get(rois, SPDB_XSETPOINTS), dtype=np.float32)
             ydata = np.array(dct_get(rois, SPDB_YSETPOINTS), dtype=np.float32)
         else:
             # use actual data
-            # xdata is teh first xnpoints
+            # xdata is the first xnpoints
             xdata = np.array(parent._data['primary'][x_src][uid]['data'][0:xnpoints], dtype=np.float32)
             # ydata is every ynpoint
             ydata = np.array(parent._data['primary'][y_src][uid]['data'][0::num_ev_points], dtype=np.float32)
@@ -176,25 +159,11 @@ def modify_line_spectra_nxdata_group(parent, data_nxgrp, doc, scan_type):
     _string_attr(data_nxgrp, 'signal', 'data')
 
     det_nm = parent.get_primary_det_nm(doc['run_start'])
+    det_data = np.array(parent._data['primary'][det_nm][uid]['data'], dtype=np.float32)
+    if (resize_data):
+        det_data = parent.fix_aborted_data(det_data, ttlpnts)
 
-    three_d_scans = [scan_types.SAMPLE_LINE_SPECTRA]
-    if(scan_types(scan_type) in three_d_scans):
-        # det_data = np.array(parent._data['primary'][det_nm]['data'], dtype=np.float32).reshape((1, ynpoints, xnpoints))
-        det_data = np.array(parent._data['primary'][det_nm][uid]['data'], dtype=np.float32)
-        # if(len(det_data) > ttl_pnts):
-        #     det_data = np.array(parent._data['primary'][det_nm][uid]['data'][0:zznpoints][0:xnpoints], dtype=np.float32)
-        # elif(len(det_data) < ttl_pnts):
-        #     print('modify_line_spectra_nxdata_group: NEED TO PAD THE DATA, length should be %d but is %d' % (len(det_data), ttl_pnts))
-
-        if (resize_data):
-            det_data = parent.fix_aborted_data(det_data, ttl_pnts)
-
-        det_data = np.reshape(det_data, (1, xnpoints, num_ev_points))
-
-    else:
-        # det_data = np.array(parent._data['primary'][det_nm]['data'], dtype=np.float32).reshape((ynpoints, xnpoints))
-        det_data = np.array(parent._data['primary'][det_nm][uid]['data'], dtype=np.float32)
-
+    det_data = np.transpose(det_data)
     _dataset(data_nxgrp, 'data', det_data, 'NX_NUMBER')
 
 
@@ -250,4 +219,4 @@ def modify_line_spectra_instrument_group(parent, inst_nxgrp, doc, scan_type):
 
     parent.make_detector(inst_nxgrp, y_posnr_nm, np.tile(ydata, ynpoints), dwell, ttl_pnts, units='um')
     parent.make_detector(inst_nxgrp, x_posnr_nm, np.tile(xdata, xnpoints), dwell, ttl_pnts, units='um')
-    #parent.make_detector(inst_nxgrp, zz_posnr_nm, np.tile(zzdata, zznpoints), dwell, ttl_pnts, units='um')
+
